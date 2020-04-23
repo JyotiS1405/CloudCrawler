9fccaba394add854ad8fa14ec65e6c92839de452
JT-SharpMindz <64171763+JT-SharpMindz@users.noreply.github.com>
2020-04-23 07:35:14 +0530

    added code for S3
    code for s3
    added static call for ajax

--- DragDrop.js
+++ DragDrop.js
@@ -1,27 +1,33 @@
+var imageCode="";
 function init()
 {
 	document.getElementById('aws_ec2').addEventListener("dragstart",ec2Drag,false);
+	document.getElementById('aws_s3').addEventListener("dragstart",s3Drag,false);
 	subcanvas=document.getElementById('subcanvas');
 	subcanvas.addEventListener("dragenter",function(e){e.preventDefault();},false);
 	subcanvas.addEventListener("dragover",function(e){e.preventDefault();},false);
-	subcanvas.addEventListener("drop",ec2Drop,false);
-}
-function ec2Drag1(img)
-{	
-img.style.top = ‘550px’;
-img.style.left = ‘550px’;
-}
-function ec2Drag(e)
-{
-	var imageCode='<img src="ec2.png" draggable=true id="temp">';
-	e.dataTransfer.setData('Aws_EC2',imageCode);
-}
-function ec2Drop(e)
-{	
-	e.preventDefault();
-	subcanvas.innerHTML=e.dataTransfer.getData('Aws_EC2');
-	img=document.getElementById('temp');
-	img.addEventListener("dragstart",ec2Drag1(img),false);
+	subcanvas.addEventListener("drop",canvasDrop,false);
 }
 
+function ec2Drag(e)
+{
+	imageCode='<img src="ec2.png" class="aws_EC2" onClick="reply_click(this.className)">';
+	e.dataTransfer.setData('Aws_EC2',imageCode);
+}
+
+function s3Drag(e)
+{
+	imageCode='<img src="s3.png" class="aws_s3" onClick="reply_click(this.className)">';
+	e.dataTransfer.setData('Aws_S3',imageCode);
+	
+}
+function canvasDrop(e)
+{	
+	e.preventDefault();
+	subcanvas.innerHTML=subcanvas.innerHTML+e.dataTransfer.getData('Aws_S3')+e.dataTransfer.getData('Aws_EC2');	
+}
+function reply_click(clicked_class)
+{
+      alert("Call AJAX to pull properties for : "+clicked_class);
+}
 window.addEventListener("load",init,false);
